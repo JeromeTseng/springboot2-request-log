89b0432d1c0af7ab0815f66dd745498d68e8e63c
曾兴顺 <zengxingshun@yunjinhz.com>
2024-07-31 15:53:25 +0800

    update: 更改Result实体类

--- src/main/kotlin/per/jerome/requestlog/model/Result.kt
+++ src/main/kotlin/per/jerome/requestlog/model/Result.kt
@@ -14,11 +14,11 @@ data class Result<T>(
 
     companion object {
         fun ok(): Result<Any?> {
-            return Result(null, 200, true, "请求成功！")
+            return Result(null, 200, true, "请求成功")
         }
 
         fun <T> ok(data: T): Result<T> {
-            return Result(data, 200, true, "请求成功！")
+            return Result(data, 200, true, "请求成功")
         }
 
         fun fail(): Result<Any?> {
